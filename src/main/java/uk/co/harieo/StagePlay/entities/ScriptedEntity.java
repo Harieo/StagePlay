a84d003d62f438b0f079e3429a744534f311cfb7
Harieo <harieo@hotmail.com>
2019-06-03 15:50:24 +0100

    Changed ScriptedEntity access modifier to package-private

--- src/main/java/uk/co/harieo/StagePlay/entities/ScriptedEntity.java
+++ src/main/java/uk/co/harieo/StagePlay/entities/ScriptedEntity.java
@@ -22,7 +22,7 @@ public class ScriptedEntity<T extends EntityInsentient> {
 	private int actionIndex = 0;
 
 	@SuppressWarnings("unchecked")
-	public ScriptedEntity(Class<T> clazz, World world, String customName) {
+	ScriptedEntity(Class<T> clazz, World world, String customName) {
 		try {
 			Constructor<?> constructor = clazz.getConstructor(net.minecraft.server.v1_12_R1.World.class);
 			entity = (T) constructor.newInstance(((CraftWorld)world).getHandle());
